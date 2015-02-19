c79ca1aa1a4391dae9c3a3c472d04025a6596de0
Paul Sinclair <bobsmith0030+git2@gmail.com>
2015-02-19 23:46:31 +0000

    maybe this time

--- static/bookedScript.js
+++ static/bookedScript.js
@@ -9,11 +9,11 @@ var currentSuggestion;
 
 //var suggestion = [];
 
-var suggestion=[{distance:0.0001433252,ratio:0.2345,coordinates:(55.2346,-3.342),capacityComp:'11',freeComp:'3',opening hours:'9-5', group:'busness', location:'busnessloc'}]; //[{roomName:'Library Cafe',latitude:55.942705,longitude:-3.189147,building:'Main Library',capacity:28,current:12, hasComputer:true,hasWhiteboard:true,hasGroupSpace:true, hasPrinter:true,openingHours:'7.30am-2.30am', type:'lab'}]
+var suggestion=[{distance:0.0001433252,ratio:0.2345,coordinates:(55.2346,-3.342),capacityComp:'11',freeComp:'3',openingHours:'9-5', group:'busness', location:'busnessloc'}]; //[{roomName:'Library Cafe',latitude:55.942705,longitude:-3.189147,building:'Main Library',capacity:28,current:12, hasComputer:true,hasWhiteboard:true,hasGroupSpace:true, hasPrinter:true,openingHours:'7.30am-2.30am', type:'lab'}]
 
 //var currentSuggestion = suggestion[0];
 
-var requests = {distance:0.0001433252,ratio:0.2345,coordinates:(55.2346,-3.342),capacityComp:'11',freeComp:'3',opening hours:'9-5', group:'busness', location:'busnessloc'}
+var requests = {distance:0.0001433252,ratio:0.2345,coordinates:(55.2346,-3.342),capacityComp:'11',freeComp:'3',openingHours:'9-5', group:'busness', location:'busnessloc'}
 
 //{distance:0.0001433252,ratio:0.2345,coordinates:(55.2346,-3.342),capacityComp:'11',freeComp:'3',opening hours:'9-5', group:'busness', location:'busnessloc'}
 
@@ -37,7 +37,7 @@ $(document).ready(function(){
 		
         getDetailed();
 		
-		displaySuggestion();
+		//displaySuggestion();
 	});
 	$('#mooore').click(function(){
 		if (!optionsExpanded){
