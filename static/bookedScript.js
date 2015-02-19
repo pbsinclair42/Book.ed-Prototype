398c17cd1eaf2dbed9de9958b09570dd7ba30759
Paul Sinclair <bobsmith0030+git2@gmail.com>
2015-02-19 20:01:01 +0000

    Made options selectable

--- static/bookedScript.js
+++ static/bookedScript.js
@@ -7,15 +7,24 @@ var gotExpanded=false;
 
 //var suggestion = [];
 
-var suggestion = [{roomName:'Library Cafe',latitude:55.942705,longitude:-3.189147,building:'Main Library',capacity:28,current:12, hasComputer:true,hasWhiteboard:true,hasGroupSpace:true, hasPrinter:true,openingHours:'7.30am-2.30am', type:'room'}]
+var suggestion = [{roomName:'Library Cafe',latitude:55.942705,longitude:-3.189147,building:'Main Library',capacity:28,current:12, hasComputer:true,hasWhiteboard:true,hasGroupSpace:true, hasPrinter:true,openingHours:'7.30am-2.30am', type:'lab'}]
 
 var currentSuggestion = suggestion[0];
 
-var requests = {close:true,quiet:true,private:false,late:true,in:'Main Library',computer:true,whiteboard:true,groupSpace:true,printer:true,suggestions:suggestion};
+var requests = {close:true,quiet:true,private:false,late:true,in:'Main Library',computer:true,whiteboard:true,groupSpace:true,printer:true,suggestions:suggestion,longitude:userLongitude,latitude:userLatitude};
 
 $(document).ready(function(){
 	getLocation();
 	//sendUserCords();
+	
+	$('.selectable').click(function(){
+		if (!$(this).hasClass('selected')){
+			$(this).addClass('selected');
+		}else{
+			$(this).removeClass('selected');
+		}
+	});
+	
     $("#yesPls").click(function(){
 		// go to booking/maps as required
 	});
@@ -27,29 +36,16 @@ $(document).ready(function(){
 		if (!optionsExpanded){
 			optionsExpanded=true;
 			$('#moreOptions').show();
-			$('#mainInterface').height(645+(inExpanded?45:0)+(gotExpanded?45:0));
+			$('#mainInterface').height(570+(inExpanded?45:0)+(gotExpanded?45:0));
 		}else{
 			optionsExpanded=false;
 			$('#moreOptions').hide();
-			$('#mainInterface').height(335);
+			$('#mainInterface').height(350);
 		}
 		
 	});
 	$("#privateBtn").click(function(){
-		alert('Suggestions of bookable tutorial rooms are not included in the alpha version'); // generate bookable room
-	});
-	$('#quietBtn').click(function(){
-		// generate empty room
-	});
-	$('#closeBtn').click(function(){
-		// generate nearby room
-		alert(userLatitude +', '+userLongitude);
-	});
-	$('#groupBtn').click(function(){
-		// generate room with group space
-	});
-	$('#openBtn').click(function(){
-		//generate room that's open for the next x hrs
+		alert('Suggestions of bookable tutorial rooms are not included in the alpha version'); //we'll include them when the timetabling department give us access to their data (hint hint pls)
 	});
 	$('#inBtn').click(function(){
 		if(!inExpanded){
@@ -57,11 +53,14 @@ $(document).ready(function(){
 			$('#inDropdown').show();
 			$('#mainInterface').height($('#mainInterface').height() + 45);
 			$('#inMenu').css({'height':'83px'});
+			$('#moreOptions').height($('#moreOptions').height()+45);
 		}else{
 			inExpanded=false;
 			$('#inDropdown').hide();
 			$('#mainInterface').height($('#mainInterface').height() - 45);
 			$('#inMenu').css({'height':'37px'});
+			$('#moreOptions').height($('#moreOptions').height()-45);
+			deselectIns();
 		}
 	});
 	$('#gotBtn').click(function(){
@@ -69,13 +68,41 @@ $(document).ready(function(){
 			gotExpanded=true;
 			$('#gotDropdown').show();
 			$('#mainInterface').height($('#mainInterface').height() + 45);
+			$('#moreOptions').height($('#moreOptions').height()+45);
 		}else{
 			gotExpanded=false;
 			$('#gotDropdown').hide();
 			$('#mainInterface').height($('#mainInterface').height() - 45);
+			$('#moreOptions').height($('#moreOptions').height()-45);
+			deselectGots();
 		}
 	})
+	$('#libraryBtn').click(function(){
+		deselectIns();
+		$(this).addClass('selected');
+	});
+	$('#centralBtn').click(function(){
+		deselectIns();
+		$(this).addClass('selected');
+	});
+	$('#kingsBtn').click(function(){
+		deselectIns();
+		$(this).addClass('selected');
+	});
 });
+
+function deselectIns(){
+	$('#libraryBtn').removeClass('selected');
+	$('#centralBtn').removeClass('selected');
+	$('#kingsBtn').removeClass('selected');
+}
+
+function deselectGots(){
+	$('#computerBtn').removeClass('selected');
+	$('#whiteboardBtn').removeClass('selected');
+	$('#groupSpaceBtn').removeClass('selected');
+	$('#printerBtn').removeClass('selected');
+}
 
 //change the display to show details of the new suggestion{
 function displaySuggestion(){
@@ -179,9 +206,7 @@ function sendUserCords() {
 
 function getDetailed(details) {
 	getLocation();
-	details.lo = userLongitude;
-	details.la = userLatitude;
-	details.details=requests;
+	details=requests;
 	$.getJSON(SCRIPT_ROOT + '/detailed_suggestion', details, function(data) {
 		//May need format change etc
 		suggestion.push(data);
